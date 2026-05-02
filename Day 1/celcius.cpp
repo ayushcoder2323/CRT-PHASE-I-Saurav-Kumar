a371d0eb726396975b268672bf937b0f50ad5f44
Ayush Singh <ayushs6875@gmail.com>
2026-05-02 09:53:24 +0530

    first commit

---
+++ Day 1/celcius.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float celsius;
+    cout<<"celsius value :";
+    cin>>celsius;
+    float farenheit = (celsius*9/5)+32;
+    cout<<"the value in farenheit :"<<farenheit<<endl;
+    return 0;
+}
