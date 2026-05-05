610243f0fdffa15ead8e54cd93efb869d17fcaf8
Ayush Singh <ayushs6875@gmail.com>
2026-05-05 09:03:02 +0530

    norm changes

--- Day 1/traingle.cpp
+++ Day 1/traingle.cpp
@@ -1,8 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a;
-    int b;
-    int c;
-    cout
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    bool isEquilateral = (a > 0) && (a == b) && (b == c);
+
+    cout << isEquilateral << endl;
+    return 0;
 }
