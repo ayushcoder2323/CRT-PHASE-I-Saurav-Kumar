612482e956f68694e0100adec5eb6643acde6c85
Ayush Singh <ayushs6875@gmail.com>
2026-05-06 09:51:03 +0530

    norm changes

--- Day 6/class.cpp
+++ Day 6/class.cpp
@@ -5,18 +5,19 @@ class student{
     string name;
     int rollno;
     int marks;
+    void display(){
+    cout<<"Name ="<<name<<endl;
+    cout<<"rollno ="<<rollno<<endl;
+    cout<<"marks ="<<marks<<endl;
+    }
 };
 int main(){
 student s1;
-cout<<"enter your name: ";
-cin>>s1.name;
-cout<<"enter your rollno: ";
-cin>>s1.rollno;
-cout<<"enter your marks: ";
-cin>>s1.marks;
-cout<<"student details:-"<<endl;
-cout<<"student name: "<<s1.name<<endl;
-cout<<"student rollno: "<<s1.rollno<<endl;
-cout<<"student marks: "<<s1.marks<<endl;
+s1.name ="Ayush";
+s1.rollno = 1846;
+s1.marks = 89;
+s1.display();
+
+
 return 0;
 }
