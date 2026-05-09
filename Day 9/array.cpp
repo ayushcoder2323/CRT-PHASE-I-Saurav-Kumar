b976c361070ef58a8a591ab6e7ac2be7cfffe1b0
Ayush Singh <ayushs6875@gmail.com>
2026-05-09 10:01:46 +0530

    normal changes

--- Day 9/array.cpp
+++ Day 9/array.cpp
@@ -1,12 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int arr[5]={1,2,3,4,5};
-    for (int i = 0;i<5;i++)
+
+int main()
+{
+    int arr[5]; //= {1, 2, 3, 4, 5};
+                // Manual input of array elements using cin
+    // cin >> arr[0];
+    // cin >> arr[1];
+    // cin >> arr[2];
+    // cin >> arr[3];
+    // cin >> arr[4];
+
+    // Direct Accessing of array elements using index and printing them using
+    // cout << arr[0] << endl;
+
+    // UserInput of array elements using loop
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < size; i++)
     {
-        cout<<arr[i]<<" ";
+        cin >> arr[i];
+    }
 
-}
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
-return 0;
+    return 0;
 }
